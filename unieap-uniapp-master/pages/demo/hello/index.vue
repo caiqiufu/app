a1e7ca89cef1cbb5ef0932e66f7eb2a575da5e97
caiqiufu <caiqiufu@gmail.com>
2024-04-18 01:20:41 +0800

    init
    init

---
+++ unieap-uniapp-master/pages/demo/hello/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<button type="primary" plain="true" @click="helloHandle">按钮</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		methods: {
+			helloHandle() {
+				console.log('hello world')
+				uni.login({
+					provider: 'weixin',
+					success:function(res){
+						console.log(res)
+					},
+					fail: function(error) {
+						console.log(error)
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
