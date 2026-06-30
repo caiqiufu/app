c7c102d395321c186375d37aa3f7a8221f882905
caiqiufu <caiqiufu@gmail.com>
2026-06-30 21:12:40 +0800

    Delete slipper-backstage-mt4 directory

--- slipper-backstage-mt4/slipper-backstage/src/main/java/com/slipper/unieap/jrds/Cache.java
+++
@@ -1,8 +0,0 @@
-package com.slipper.unieap.jrds;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Cache {
-	public static Map<String,Long> executeTime = new HashMap<String,Long>();
-}
